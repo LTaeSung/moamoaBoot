a6227404695d3464b4ae5e463e916b514966d9cc
powerpress0809 <powerpress0809@naver.com>
2024-02-07 20:14:57 +0900

    2024-02-07 친구 검색 추가 삭제 기능 구현 (신정훈)

--- src/main/java/don/us/member/FriendController.java
+++ src/main/java/don/us/member/FriendController.java
@@ -1,12 +1,14 @@
 package don.us.member;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,7 @@ public class FriendController {
 	@Autowired
 	private MemberRepository meRepo;
 	
+	
 	// 친구 정보 불러오기 (신정훈 작업 2024 - 02 - 07)
 	@GetMapping("/list")
 	public List friendList(@RequestParam("member_no") int member_no) {
@@ -34,8 +37,8 @@ public class FriendController {
 	}
 	
 	// 친구 추가 기능 (신정훈 작업 2024 - 02 - 07)	
-	@PostMapping("/input")
 	@Transactional
+	@PostMapping("/input")
 	public FriendEntity friendAdd(@RequestParam("member_no") int member_no, @RequestParam("friend_no") int friend_no) {
 		
 		FriendEntity friend = new FriendEntity();
@@ -49,5 +52,17 @@ public class FriendController {
 		return entity;
 	}
 	
-
+	// 친구 삭제 기능 (신정훈 작업 2024 - 02 - 07)	
+	@Transactional
+	@PostMapping("/delete")
+	public Map friendDel(@RequestParam("member_no") int member_no, @RequestParam("friend_no") int friend_no){
+		
+		frRepo.findByMemberno(member_no);
+		frRepo.deleteByFriendNo(friend_no);
+		
+		Map<String, Object> result = new HashMap<>();
+		result.put("result", "success");
+		return result;
+		
+	}
 }
