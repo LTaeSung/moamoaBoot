255d5a142df55b84ea2cbd932cbe7a0c9ebc221d
dlxotjds2 <dlxotjds2@naver.com>
2024-02-08 17:51:08 +0900

    fundin

--- src/main/java/don/us/funding/FundingController.java
+++ src/main/java/don/us/funding/FundingController.java
@@ -52,11 +52,6 @@ public class FundingController {
 		fundingEntity.setDescription((String) map.get("description"));
 //		fundingEntity.setPhoto((String) map.get("photo")); // 추후에 추가 예정
 		fundingEntity.setCandidate((Integer) map.get("candidate"));
-		fundingEntity.setExpected_payment_amount((Integer) map.get("expected_payment_amount"));
-		fundingEntity.setGoal_amount((Integer) map.get("goal_amount"));
-		fundingEntity.setMonthly_payment_amount((Integer) map.get("monthly_payment_amount"));
-		fundingEntity.setMonthly_payment_date((Integer) map.get("monthly_payment_date"));
-		fundingEntity.setComplete_interest((Integer) map.get("complete_interest"));
 
 		FundingEntity entity = repo.save(fundingEntity);
 		Map<String, Object> result = new HashMap<>();
