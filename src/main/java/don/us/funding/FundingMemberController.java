121726786dbc918af85db08b606377943d66936f
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-13 21:48:36 +0900

    초대일 - 현재일로 날짜 계산해야함

--- src/main/java/don/us/funding/FundingMemberController.java
+++ src/main/java/don/us/funding/FundingMemberController.java
@@ -1,13 +1,13 @@
 package don.us.funding;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +30,12 @@ public class FundingMemberController {
 			if(fund.getParticipationdate() != null) {
 				continue;
 			}
+			Timestamp invitedDate = fund.getInviteddate();
+			Calendar cal = Calendar.getInstance();
 			result.add(fund);
 		}
 		
 		System.out.println("result: " + result);
-		return result;
+		return allFundOfMe;
 	}
 }
