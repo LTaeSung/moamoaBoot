bab72633b3b976220ea239a89dcc1a31f20cc73b
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-13 15:12:38 +0900

    펀드 개시 사진등록까지 끝. 알림보내기하면됨

--- src/main/java/don/us/funding/FundingController.java
+++ src/main/java/don/us/funding/FundingController.java
@@ -1,21 +1,26 @@
 package don.us.funding;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import util.file.FileController;
+import util.file.FileNameVO;
 
 @CrossOrigin(origins = { "*" })
 @RestController
@@ -23,13 +28,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class FundingController {
 	@Autowired
 	private FundingRepository repo;
-
+	@Autowired
+	private FileController fileController;
 	@Autowired
 	private FundingService service;
-
+	
+	@Value("${realPath.registed_img_path}")
+	private String registed_img_path;
+	
 	@PostMapping("/regist")
-	public void makeFund(@RequestBody Map map) {
-		System.out.println(map);
+	public void makeFund(@RequestParam Map map, @RequestParam(name = "file", required = false) MultipartFile photo) {
+//		System.out.println("map: " + map);
+		System.out.println("photo: " + photo);
 		FundingEntity fund = new FundingEntity();
 
 		fund.setStartmemberno(Integer.valueOf((String) (map.get("member_no"))));
@@ -39,15 +49,44 @@ public class FundingController {
 		fund.setMonthlypaymentamount(Integer.valueOf((String) (map.get("monthly_payment_amount"))));
 		fund.setMonthlypaymentdate((String)map.get("monthly_payment_date"));
 		
-		String dueDate = ((String)map.get("dueDate")).substring(0, 10);
-		
-	    LocalDate localDate = LocalDate.parse(dueDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-		fund.setFundingduedate(Timestamp.valueOf(localDate.atStartOfDay()));
-		System.out.println("fund: " + fund);
+		String dueDateString = (String)map.get("dueDate");
+		SimpleDateFormat inputFormat = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss 'GMT'Z", java.util.Locale.ENGLISH);
+		try {
+			Date date = inputFormat.parse(dueDateString);
+            Timestamp timestamp = new Timestamp(date.getTime());
+            fund.setFundingduedate(timestamp);
+		}catch(ParseException e) {
+			e.printStackTrace();
+		}
 
-		// 임시로 payment_no를 1로 설정
-		int payment_no = 1;
+		if(photo != null) {
+			FileNameVO fvo = fileController.upload(photo, registed_img_path);
+			fund.setPhoto(fvo.getSaved_filename());
+		}
+		
+		
 		repo.save(fund);
+		System.out.println("fund: " + fund);
+		System.out.println("map: " + map);
+
+//		// 임시로 payment_no를 1로 설정
+		int payment_no = 1;
+		FundingMemberEntity me = service.makeFundingMemberEntity(fund, fund.getStartmemberno());
+		me.setPaymentno(payment_no);
+		me.setParticipation_date(new Timestamp(System.currentTimeMillis()));
+		service.inviteMember(fund, me);
+		
+		if(map.get("memberList") != null) {
+			List<String> memberList = Arrays.asList(((String)map.get("memberList")).split(","));
+			System.out.println("memberList: " + memberList);
+			
+			
+			for(String i : memberList) {
+				int member_no = Integer.valueOf(i);
+				service.inviteMember(fund, service.makeFundingMemberEntity(fund, member_no));
+			}
+		}
+
 	}
 	@GetMapping("/list")
 	public List index (Model model) {
@@ -56,19 +95,6 @@ public class FundingController {
 
 		return fundingEntityList;
 	}
-	@PostMapping("/reg")
-	public Map<String, Object> fundList(@RequestBody Map map){
-		FundingEntity fundingEntity = new FundingEntity();
-		fundingEntity.setNo(Integer.parseInt((String)map.get("no")));
-		fundingEntity.setTitle((String) map.get("title"));
-		fundingEntity.setDescription((String) map.get("description"));
-//		fundingEntity.setPhoto((String) map.get("photo")); // 추후에 추가 예정
-		fundingEntity.setCandidate((Integer) map.get("candidate"));
 
-		FundingEntity entity = repo.save(fundingEntity);
-		Map<String, Object> result = new HashMap<>();
-		result.put("entity", entity);
-		result.put("result", entity == null ? "fail" : "success");
-		return result;
-	}
+	
 }
