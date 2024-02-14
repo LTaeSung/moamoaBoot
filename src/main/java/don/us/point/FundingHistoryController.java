28a4f0c3ad4c101bc1b27af201cd0e729ebf6aa9
jhyoo <jhyoo1224@gmail.com>
2024-02-14 15:07:08 +0900

    포인트 기록 불러오기 완료

--- src/main/java/don/us/point/FundingHistoryController.java
+++ src/main/java/don/us/point/FundingHistoryController.java
@@ -1,8 +1,12 @@
 package don.us.point;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = {"*"})
@@ -11,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class FundingHistoryController {
 	@Autowired
 	private FundingHistoryRepository repo;
+	
+	@GetMapping("/mypointHistory")
+    public List<FundingHistoryEntity> pointHistory(@RequestParam(value="member_no") int member_no) {
+    	List<FundingHistoryEntity> pointList = repo.findByMemberno(member_no);
+    	return pointList;
+    }
 }
