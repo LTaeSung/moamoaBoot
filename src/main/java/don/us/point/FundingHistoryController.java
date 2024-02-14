a7c0cd7a64b057e04e93be2b58ecda0d9fa6eaf5
jhyoo <jhyoo1224@gmail.com>
2024-02-14 19:23:23 +0900

    save때문에 transactinal 어노테이션 추가

--- src/main/java/don/us/point/FundingHistoryController.java
+++ src/main/java/don/us/point/FundingHistoryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class FundingHistoryController {
     	return pointList;
     }
 	
+	@Transactional
 	@GetMapping("/regularPayment")
 	public void regularPayment() {
 		ArrayList<List<FundingMemberEntity>> list = fundingService.needPayMemberList();
