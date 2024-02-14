2dab34d058a727d855ea74a96a47a1593025a733
powerpress0809 <powerpress0809@naver.com>
2024-02-14 18:24:49 +0900

    피쏠린다...

--- src/main/java/don/us/funding/FundingMemberController.java
+++ src/main/java/don/us/funding/FundingMemberController.java
@@ -3,6 +3,7 @@ package don.us.funding;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,12 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.EntityManager;
+
+
+
 @CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/funding/member")
 public class FundingMemberController {
 	@Autowired
 	private FundingMemberRepository repo;
+	
+	@Autowired
+	private EntityManager entityManager;
 	
 	@GetMapping("invitedList")
 	public List<FundingMemberEntity> getInvitedList(@RequestParam("member_no") int member_no) {
@@ -89,5 +97,27 @@ public class FundingMemberController {
 			System.out.println("삭제 실패");
 			return "fail";
 		}
+	}
+	
+	
+	@GetMapping("/join")
+	public List<FundingMemberEntity> joinList (@RequestParam("member_no") int member_no){
+		
+		String jpql ="SELECT fm FROM FundingMemberEntity fm "
+				+ "WHERE fm.memberno = :member_no AND fm.startmemberno != :start_member_no";
+		
+		List<FundingMemberEntity> joinList = entityManager.createQuery(jpql,FundingMemberEntity.class)
+															.setParameter("member_no" , member_no)
+															.setParameter("start_member_no" , member_no)
+															.getResultList();
+		if (joinList.isEmpty()) {
+			System.out.println("참여한 모금이 없습니다.");
+		}else {
+			System.out.println("모금 리스트: " + joinList);
+		}
+		
+		return joinList;
+		
+		
 	}
 }
