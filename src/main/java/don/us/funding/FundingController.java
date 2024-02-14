18cdedc0d8045ef7f33b61ca2d6d49053dfd85e0
powerpress0809 <powerpress0809@naver.com>
2024-02-14 16:35:03 +0900

    본인이 주최한 펀딩리스트 가져오기

--- src/main/java/don/us/funding/FundingController.java
+++ src/main/java/don/us/funding/FundingController.java
@@ -78,36 +78,23 @@ public class FundingController {
 	}
 
 	@GetMapping("/list")
-	public List<FundingEntity> myFunding(@RequestParam("start_member_no") int startmemberno) {
+	public List<FundingEntity> myFunding(@RequestParam("start_member_no") int start_member_no) {
 		
-		List <FundingEntity> myFundinglist = repo.findBystartmemberno(startmemberno);
+		List <FundingEntity> myFundinglist = repo.findBystartmemberno(start_member_no);
 		
 		return myFundinglist;
 	}
-	
-//	@GetMapping("/list")
-//	public List index(Model model) {
-//
-//		List<FundingEntity> fundingEntityList = repo.findAll();
-//
-//		return fundingEntityList;
-//	}
-//
-//	@GetMapping("/list/{no}")
-//		public ResponseEntity<FundingEntity> show(@PathVariable int no) {
-//			Optional<FundingEntity> optionalFundingEntity = repo.findById(no);
-//			return optionalFundingEntity.map(ResponseEntity::ok).orElseGet(() ->
-//					ResponseEntity.notFound().build());
-//		}
-//	@GetMapping("/list/{no}")
-//	public List<FundingEntity> show(@PathVariable int no) {
-//		List<FundingEntity> result = new ArrayList<>();
-//		repo.findById(no).ifPresent((data) -> {
-//			result.add(data);
-//		});
-//		System.out.println("result: " + result);
-//		return result;
-//	}
+
+
+	@GetMapping("/list/{no}")
+	public List<FundingEntity> show(@PathVariable int no) {
+		List<FundingEntity> result = new ArrayList<>();
+		repo.findById(no).ifPresent((data) -> {
+			result.add(data);
+		});
+		System.out.println("result: " + result);
+		return result;
+	}
 	
 	
 }
