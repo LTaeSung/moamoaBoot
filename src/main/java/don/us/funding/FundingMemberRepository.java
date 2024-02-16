3e0a3de38709ff2d896f44b0c7f4e4d546c945d7
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-16 15:13:34 +0900

    현재 참여중이고 진행중인 펀딩 끝

--- src/main/java/don/us/funding/FundingMemberRepository.java
+++ src/main/java/don/us/funding/FundingMemberRepository.java
@@ -20,14 +20,14 @@ public interface FundingMemberRepository extends JpaRepository<FundingMemberEnti
 	Optional<FundingMemberEntity> findByFundingnoAndMemberno(int funding_no, int member_no);
 
 	
-	String queryForOnGoingFunding = """
+	@Query(value="""
 			select
 				m.no as fundingMemberNo,
 				f.no as fundingNo,
 				f.title as fundingTitle,
 				m.monthlypaymentdate as monthlyPaymentDate,
 				m.monthlypaymentamount as monthlyPaymentAmmount,
-
+	
 				
 				m.totalpayamount as myPayAmount,
 				f.collectedpoint as totalPayAmount,
@@ -50,7 +50,6 @@ public interface FundingMemberRepository extends JpaRepository<FundingMemberEnti
 				and
 				f.state != 4
 			order by f.state desc
-			""";
-	@Query(value=queryForOnGoingFunding)
+		""")
 	public List<Map> getJoinedFundingList_OnGoing(String member_no);
 }
