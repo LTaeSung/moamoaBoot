f3e41eb6fbe1b56ccef908324b4c0619f2286c69
goeunkim <ak0150a01@gmail.com>
2024-02-16 16:47:13 +0900

    2

--- src/main/java/don/us/funding/FundingCommentRepository.java
+++ src/main/java/don/us/funding/FundingCommentRepository.java
@@ -1,6 +1,11 @@
 package don.us.funding;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface FundingCommentRepository extends JpaRepository<FundingCommentEntity, Integer>{
+	 List<FundingCommentEntity> findByfundingno(int fundingno);
+	public void deleteByfundingno(int fundingno);
 }
