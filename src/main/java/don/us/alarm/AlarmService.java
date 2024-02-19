f0ba214a104cba92072ba20ea55cf6d99eec59e7
jhyoo <jhyoo1224@gmail.com>
2024-02-19 11:27:21 +0900

    펀딩상태 2에서 3으로 바꾸는거 test까지 완(나중에 데이터 제대로 넣어서 돌려보긴 해야함), 알람서비스 함수 분리

--- src/main/java/don/us/alarm/AlarmService.java
+++ src/main/java/don/us/alarm/AlarmService.java
@@ -15,39 +15,65 @@ public class AlarmService {
 	@Autowired
 	private AlarmRepository repo;
 	
+	public void makeAlarm(int memberno, String content, String link, int state) {
+		AlarmEntity alarm = new AlarmEntity();
+		alarm.setMemberno(memberno);
+		alarm.setContent(content);
+		alarm.setLink(link);
+		alarm.setState(state);
+		repo.save(alarm);
+	}
+	
 	public void makeInviteAlarm(FundingMemberEntity fundingMember) {
 		String content = "" + fundingMember.getStartmembername() + "님이 " + "[" + fundingMember.getFundtitle()
 				+ "]에 초대하셨습니다.";
 		String link = "/funding/invited";
 
-		AlarmEntity alarm = new AlarmEntity();
-		alarm.setMemberno(fundingMember.getMemberno());
-		alarm.setContent(content);
-		alarm.setLink(link);
-		alarm.setState(0);
-		
-		repo.save(alarm);
+//		AlarmEntity alarm = new AlarmEntity();
+//		alarm.setMemberno(fundingMember.getMemberno());
+//		alarm.setContent(content);
+//		alarm.setLink(link);
+//		alarm.setState(0);
+//		repo.save(alarm);
+		makeAlarm(fundingMember.getMemberno(), content, link, 0);
 	}
 	
 	
 	public void makePayAlarm(int memberno, String content, int fundingno) {
 		String link = "/funding/info?no="+fundingno;
 		
-		AlarmEntity alarm = new AlarmEntity();
-		alarm.setMemberno(memberno);
-		alarm.setContent(content);
-		alarm.setLink(link);
-		repo.save(alarm);
+//		AlarmEntity alarm = new AlarmEntity();
+//		alarm.setMemberno(memberno);
+//		alarm.setContent(content);
+//		alarm.setLink(link);
+//		alarm.setState(0);
+//		repo.save(alarm);
+		makeAlarm(memberno, content, link, 0);
 	}
 	
 	public void makeVoteAlarm(int memberno, int fundingno) {
 		String content = "참여하신 챌린지가 종료되었습니다. 챌린지 성공/실패 여부를 체크해주세요. 일주일 내에 입력하지 않으실 경우, 자동으로 실패 처리됩니다.";
 		String link = "/funding/info?no="+fundingno;
 		
-		AlarmEntity alarm = new AlarmEntity();
-		alarm.setMemberno(memberno);
-		alarm.setContent(content);
-		alarm.setLink(link);
-		repo.save(alarm);
+//		AlarmEntity alarm = new AlarmEntity();
+//		alarm.setMemberno(memberno);
+//		alarm.setContent(content);
+//		alarm.setLink(link);
+//		alarm.setState(0);
+//		repo.save(alarm);
+		makeAlarm(memberno, content, link, 0);
+	}
+	
+	public void makeSettlmentAlarm(int memberno, int fundingno) {
+		String content = "참여하신 챌린지의 투표가 종료되었습니다. 정산금을 받아가세요.";
+		String link = "/funding/info?no="+fundingno;
+		
+//		AlarmEntity alarm = new AlarmEntity();
+//		alarm.setMemberno(memberno);
+//		alarm.setContent(content);
+//		alarm.setLink(link);
+//		alarm.setState(0);
+//		repo.save(alarm);
+		makeAlarm(memberno, content, link, 0);
 	}
 }
--- src/test/java/don/us/admin/AdminControllerTest.java
+++ src/test/java/don/us/admin/AdminControllerTest.java
@@ -75,18 +75,23 @@ public class AdminControllerTest {
 	@Transactional
 	@Test
 	public void setFundStatus1To2() {
+		System.out.println("실행됨");
 		//펀드 상태=1 and 펀드 마감일<now()인 목록 가져옴
 		List<FundingEntity> fundlist = fundingRepo.getFundingDueList();
+		System.out.println("펀딩 개수: "+fundlist.size());
 		for(int i=0; i<fundlist.size(); i++) {
 			//펀드 상태를 1->2로 업뎃, 투표 마감일을 마감일(funding_due_date)+7일 해서 넣어줌
+			System.out.println("펀드상태 업뎃전 확인"+fundlist.get(i).getState()+" 펀드 투표기한 들어가기전 확인: "+fundlist.get(i).getVoteduedate());
 			fundlist.get(i).setState(2);
 			fundlist.get(i).setVoteduedate(handleDays.addDays(fundlist.get(i).getFundingduedate(), 7));
+			System.out.println("펀드상태 업뎃 확인"+fundlist.get(i).getState()+" 펀드 투표기한 잘 들어갔나 확인: "+fundlist.get(i).getVoteduedate());
 			fundingRepo.save(fundlist.get(i));
 			
 			//해당 펀딩 참여자중에 중도포기 안 한(giveup=false) 사람들 목록 가져와서 투표하라고 알림보냄
 			List<FundingMemberEntity> completeMemberList = fundingMemberRepo.getCompleteMemberList(fundlist.get(i).getNo());
 			for(int j=0; j<completeMemberList.size(); j++) {
 				alarmService.makeVoteAlarm(completeMemberList.get(j).getMemberno(), fundlist.get(i).getNo());
+				System.out.println("알람갔어용");
 			}
 		}	
 	}
@@ -94,30 +99,77 @@ public class AdminControllerTest {
 	@Transactional
 	@Test
 	public void setFundStatus2To3() {
+		System.out.println("실행됨");
 		//펀드상태=2 and 투표마감일<now()인 목록 가져옴
 		List<FundingEntity> fundlist = fundingRepo.getVoteDueList();
+		System.out.println("펀딩 개수: "+fundlist.size());
 		//멤버 중 투표 안 한 인원이 있으면(vote=0) 전부 실패(2)로 업뎃
 		for(int i=0; i<fundlist.size(); i++) {
 			List<FundingMemberEntity> dontVoteMemberList = fundingMemberRepo.needVoteFundMemberList(fundlist.get(i).getNo());
+			
 			for(int j=0; j<dontVoteMemberList.size(); j++) {
-				dontVoteMemberList.get(j).setVote(2);
-				fundingMemberRepo.save(dontVoteMemberList.get(j));
+				vote(dontVoteMemberList.get(j), 2);
+				if(checkVoteIsComplete(dontVoteMemberList.get(j).getFundingno())) {
+					computeAndSetSettlementAccount(fundlist.get(i));
+				}
 			}
-			//성공한 멤버들만 불러와서 전체모금액/list.size해 인당 정산금 구한 뒤, 펀딩의 1인당정산금액에 저장, 성공자들한테만 정산받으라고 알림
-			List<FundingMemberEntity> successMemberList = fundingMemberRepo.getsuccessFundMemberList(fundlist.get(i).getNo());
-			if(successMemberList.size() == 0) {
-				//만약 성공한 멤버 없으면(전체실패, list.size=0) 그냥 완주한 전체 인원(=candidate)으로 나누고 똑같이 처리, 전체 알림
-				
-			} else {
-				
+			//펀드 status 3으로 업뎃~
+			System.out.println("상태 업뎃 전: "+fundlist.get(i).getState());
+			fundlist.get(i).setState(3);
+			fundingRepo.save(fundlist.get(i));
+			System.out.println("상태 업뎃 후: "+fundlist.get(i).getState());
+		}
+	}
+	
+	@Test
+	//투표한 FundingMemberEntity 넣어주고, 몇 번으로 투표했는지 넣어주세용(1=성공, 2=실패)
+	public void vote(FundingMemberEntity member, int result) {
+		System.out.println("투표 제대로 들어가나 확인(세팅전) "+member.getVote());
+		member.setVote(result);
+		System.out.println("투표 제대로 들어가나 확인(세팅후) "+member.getVote());
+		fundingMemberRepo.save(member);
+	}
+	@Test //해당 펀딩에서 투표 안 한 사람 있는지 확인
+	public boolean checkVoteIsComplete(int fundingno) {
+		List<FundingMemberEntity> dontVoteMemberList = fundingMemberRepo.needVoteFundMemberList(fundingno);
+		if(dontVoteMemberList.size() == 0) {System.out.println("확인 true"); return true;}
+		else {System.out.println("확인 false"); return false;}
+	}
+	@Test //전원 투표했으면 그 결과로 정산금액 계산, 알람보내줌
+	public void computeAndSetSettlementAccount(FundingEntity fund) {
+		List<FundingMemberEntity> successMemberList = fundingMemberRepo.getsuccessFundMemberList(fund.getNo());
+		if(successMemberList.size() == 0) {
+			System.out.println("성공멤버 없음");
+			//만약 성공한 멤버 없으면(전체실패, list.size=0) 그냥 완주한 전체 인원(=candidate)으로 나누고 똑같이 처리, 전체 알림
+			int will_settlement_amount = fund.getCollectedpoint() / fund.getCandidate();
+			List<FundingMemberEntity> allMemberList = fundingMemberRepo.needPayFundMemberList(fund.getNo());
+			System.out.println("정산금 확인, 총 금액 : "+fund.getCollectedpoint()+" 인당 금액: "+will_settlement_amount);
+			for(int i=0; i<allMemberList.size(); i++) {
+				setAndAlarmSettlementAccount(allMemberList.get(i), will_settlement_amount);
+			}
+		} else { //성공한 멤버 있으면 성공한 멤버한테만
+			System.out.println("성공멤버 있음 "+successMemberList.size()+" 명");
+			int will_settlement_amount = fund.getCollectedpoint() / successMemberList.size();
+			System.out.println("정산금 확인, 총 금액 : "+fund.getCollectedpoint()+" 인당 금액: "+will_settlement_amount);
+			for(int i=0; i<successMemberList.size(); i++) {
+				System.out.println("정산금 제대로 들어가나 확인(세팅전) "+successMemberList.get(i).getWillsettlementamount());
+				setAndAlarmSettlementAccount(successMemberList.get(i), will_settlement_amount);
+				System.out.println("정산금 제대로 들어가나 확인(세팅후) "+successMemberList.get(i).getWillsettlementamount());
 			}
 		}
-		//펀드 status 3으로 업뎃~
+	}
+	@Test //computeAndSetSettlementAccount에서 호출할 정산예정금 저장 및 알림보내는 함수
+	public void setAndAlarmSettlementAccount(FundingMemberEntity member, int amount) {
+		member.setWillsettlementamount(amount);
+		fundingMemberRepo.save(member);
+		alarmService.makeSettlmentAlarm(member.getMemberno(), member.getFundingno());
 	}
 	
 	@Test
 	public void setFundStatus3To4() {
 		//
 	}
-	
+	//정산받기
+	//체크
+	//남은인원없으면 종료상태로 이전
 }
