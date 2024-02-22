2e457137eb83628448d0e5252512763daa90a8fc
goeunkim <ak0150a01@gmail.com>
2024-02-22 13:49:02 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/SinhanDS-moamoaDonDon/moamoa

--- src/main/java/don/us/board/BoardRepository.java
+++ src/main/java/don/us/board/BoardRepository.java
@@ -1,9 +1,11 @@
 package don.us.board;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import don.us.funding.FundingCommentEntity;
+
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer>{
-	
+	List<BoardEntity> findByBoardtype(boolean boardtype);
 }
